aabf074fdb430493d4777c362a044ddd23defecc
Pauline-Oraro <hiltrapauline@gmail.com>
2025-08-23 17:08:04 +0300

    adding create post page route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { Route, Routes } from "react-router"
 import Home from "./pages/Home"
 import NavBar from "./components/NavBar"
+import CreatePostPage from "./pages/CreatePostPage"
 
 const App = () => {
   return(
@@ -10,7 +11,7 @@ const App = () => {
       <div className="container mx-auto px-4 py-6">
         <Routes>
           <Route path="/" element={<Home />} />
-
+          <Route path="/create" element={<CreatePostPage />} />
         </Routes>
       </div>
     </div>
