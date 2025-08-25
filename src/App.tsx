f9dacc7b0425239d08a6376f3e87540829a1c8d9
Pauline-Oraro <hiltrapauline@gmail.com>
2025-08-25 17:52:34 +0300

    adding a route for the community page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home"
 import NavBar from "./components/NavBar"
 import CreatePostPage from "./pages/CreatePostPage"
 import PostPage from "./pages/PostPage"
+import CreateCommunity from "./components/CreateCommunity"
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<CreatePostPage />} />
           <Route path="/post/:id" element={<PostPage />} />
+          <Route path="/community/create" element={<CreateCommunity />} />
         </Routes>
       </div>
     </div>
